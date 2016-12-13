3bf30f92d37ebe7769d409fae3c267bfd1484407
Podbi <podbi@centrum.cz>
2016-12-13 14:33:26 +0100

    product-search: Initiali drivers & Controller

---
+++ src/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+class ProductController
+{
+    /**
+     * @param string $id
+     * @return string
+     */
+    public function detail($id)
+    {
+        // do stuff and return json
+    }
+}
